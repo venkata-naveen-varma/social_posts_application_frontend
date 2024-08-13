9f5fb435c56565e952fddca7c09483e8859c898c
Venkata Naveen Varma Vegesna <115595566+venkata-naveen-varma@users.noreply.github.com>
2024-08-13 12:16:34 -0400

    update server API

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://salmon-prawn-boot.cyclic.app/posts';
+const url = 'https://social-posts-application.onrender.com/';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
